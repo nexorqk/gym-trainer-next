74c85c01534923a6107fe857b9c96c96f6a68fa5
nexorqk <pkms2a10@gmail.com>
2024-05-30 13:37:32 +0300

    wip

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,24 @@
-import { SignUp } from "@clerk/nextjs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { SignUpTrainer } from "./forms";
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <Tabs defaultValue="account">
+      <TabsList className="w-full">
+        <TabsTrigger className="w-60" value="account">
+          Account
+        </TabsTrigger>
+        <TabsTrigger className="w-60" value="password">
+          Password
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="account"
+        className="mx-auto max-w-[600px] flex flex-col items-center space-y-3"
+      >
+        <SignUpTrainer />
+      </TabsContent>
+      <TabsContent value="password">Change your password here.</TabsContent>
+    </Tabs>
+  );
 }
